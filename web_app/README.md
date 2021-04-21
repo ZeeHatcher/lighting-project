2421b265356859494dd8cf6dd6d0400ef7ef9a67
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 21:44:17 +0800

    add dotenv library, .env configuration, and setup steps

---
+++ web_app/README.md
@@ -0,0 +1,11 @@
+## Libraries
+* MySQLdb
+* dotenv
+* flask
+
+## Setup
+1. In the terminal:
+   1. Run `sudo apt-get install python-flask python-mysqldb`
+   1. Run `sudo pip install python-dotenv`
+1. Copy _.env\_example_ into _.env_
+1. Update values inside _.env_ to work with your environment
--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,19 @@
 import json
 import MySQLdb
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_DATABASE = os.environ.get("DB_DATABASE")
+
 app = Flask(__name__)
-#db = MySQLdb.connect("localhost", "pi", "", "DB_HERE") or die("Could not connect to database")
-#db.autocommit(True)
+db = MySQLdb.connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect to database")
 
 @app.route("/")
 def index():
