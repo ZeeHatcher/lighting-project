f4057f1fc634630d503356e90d4e54db67dfbe31
ZeeHatcher <zee.hatcher010@gmail.com>
2021-04-24 21:31:38 +0800

    add virtual lightstick; update gitignore

---
+++ edge/virtual.py
@@ -0,0 +1,45 @@
+from graphics import *
+
+class VirtualLightstick:
+    PADDING = 2
+    PIXEL_SIZE = 4
+
+    def __init__(self, num_pixels):
+        self._num_pixels = num_pixels
+
+        # Initialize window object
+        win = GraphWin("Virtual Lightstick",
+            ((VirtualLightstick.PADDING * 2) + num_pixels) * VirtualLightstick.PIXEL_SIZE, # width
+            ((VirtualLightstick.PADDING * 2) + 1) * VirtualLightstick.PIXEL_SIZE # height
+        )
+        win.setBackground("black")
+        win.autoflush = False
+        self._win = win
+
+        # Initialize "pixels"
+        pixels = []
+        y = VirtualLightstick.PADDING * VirtualLightstick.PIXEL_SIZE
+        for i in range(num_pixels):
+            x = (VirtualLightstick.PADDING + i) * VirtualLightstick.PIXEL_SIZE
+            p1 = Point(x, y)
+            p2 = Point(x + VirtualLightstick.PIXEL_SIZE, y + VirtualLightstick.PIXEL_SIZE)
+
+            pixels.append(Rectangle(p1, p2))
+            pass
+        
+        self._pixels = pixels
+
+    def update(self, c_r, c_g, c_b):
+        for i in range(self._num_pixels):
+            color = color_rgb(
+                int(c_r[i].encode("hex"), 16),
+                int(c_g[i].encode("hex"), 16),
+                int(c_b[i].encode("hex"), 16)
+            )
+
+            pixel = self._pixels[i]
+            pixel.undraw()
+            pixel.setFill(color)
+            pixel.draw(self._win)
+
+        self._win.update()
